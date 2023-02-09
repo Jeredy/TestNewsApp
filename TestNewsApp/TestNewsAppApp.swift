f1192e7870192f6492bc5b6607f6b19e47f8ff6d
Andre Rodrigues <andrecomerciale@gmail.com>
2023-02-09 11:13:53 -0300

    Initial Commit

---
+++ TestNewsApp/TestNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppApp.swift
+//  TestNewsApp
+//
+//  Created by André Almeida on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
